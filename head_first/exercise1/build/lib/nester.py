aadf7a1ea5a0e0cb818602e1aaf7ffee0bcbb385
James Nguyen <james.nguyen@hp.com>
2022-04-06 03:23:42 -0700

    Removed head first dir

--- head_first/exercise1/build/lib/nester.py
+++
@@ -1,8 +0,0 @@
-
-# Goes through a list, and if there are any nested lists, prints items individually
-def print_lol(some_list):
-    for item in some_list:
-        if isinstance(item, list):
-            print_lol(item) #recursive
-        else:
-            print(item)
--- head_first/exercise1/chapter2.py
+++
@@ -1,4 +0,0 @@
-import nester
-
-cast = ["Palin", "Cleese", "Idle", "Jones", "Gilliam", "Chapman"]
-nester.print_lol(cast)
